e9b2d6861243bd910a19131e3ed24306807943f3
manmythmardo <alexc.mardoqueo@gmail.com>
2023-04-30 11:54:29 -0500

    Fences Work on Area Target Now

--- XRStarterKit/Assets/XRStarterKit/Scripts/FenceSpawner.cs
+++ XRStarterKit/Assets/XRStarterKit/Scripts/FenceSpawner.cs
@@ -1,64 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Normal.Realtime;
 public class FenceSpawner : MonoBehaviour
 {
     public GameObject fencePostPrefab;
     public GameObject fenceConnectionPrefab;
     public Material fenceMaterial;
     public int maxFencePosts = 5;
+    public LayerMask groundLayer;
     private int fencePostCount = 0;
     private GameObject lastFencePost = null;
-
-    void Update()
-    {
+    private Realtime _realtime;
+    void Update(){
         if (Input.GetMouseButtonDown(0) && fencePostCount < maxFencePosts)
         {
-            // Get the position of the mouse click in world space
-            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            clickPosition.y = 0f; // Ensure fence post is spawned at ground level
-
-            // Instantiate a new fence post at the clicked position
-            GameObject newFencePost = Instantiate(fencePostPrefab, clickPosition, Quaternion.identity);
-
-            // Assign the fence material to the fence post's mesh renderer
-            MeshRenderer fencePostRenderer = newFencePost.GetComponent<MeshRenderer>();
-            if (fencePostRenderer != null)
-            {
-                fencePostRenderer.material = fenceMaterial;
-            }
-
-            // Increment the fence post count
-            fencePostCount++;
-
-            // If this is not the first fence post, create a connection to the previous one
-            if (lastFencePost != null)
-            {
-                // Calculate the midpoint between the last fence post and the new one
-                Vector3 midPoint = (lastFencePost.transform.position + newFencePost.transform.position) / 2f;
-
-                // Instantiate a new fence connection at the midpoint
-                GameObject newFenceConnection = Instantiate(fenceConnectionPrefab, midPoint, Quaternion.identity);
-
-                // Rotate the fence connection to face the last fence post
-                Vector3 direction = lastFencePost.transform.position - newFencePost.transform.position;
-                newFenceConnection.transform.rotation = Quaternion.LookRotation(direction);
-
-                // Scale the fence connection to fit between the two fence posts
-                float distance = Vector3.Distance(lastFencePost.transform.position, newFencePost.transform.position);
-                newFenceConnection.transform.localScale = new Vector3(0.2f, .4f, distance);
-
-                // Assign the fence material to the fence connection's mesh renderer
-                MeshRenderer fenceConnectionRenderer = newFenceConnection.GetComponent<MeshRenderer>();
-                if (fenceConnectionRenderer != null)
-                {
-                    fenceConnectionRenderer.material = fenceMaterial;
+            Vector3 clickPosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(clickPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer)){
+                GameObject newFencePost = Realtime.Instantiate(fencePostPrefab.name, hit.point, Quaternion.identity,true); 
+                MeshRenderer fencePostRenderer = newFencePost.GetComponent<MeshRenderer>();
+                if (fencePostRenderer != null){
+                    fencePostRenderer.material = fenceMaterial;
                 }
-            }
 
-            // Remember this fence post for the next connection
-            lastFencePost = newFencePost;
+                fencePostCount++;
+                if (lastFencePost != null){
+                    Vector3 midPoint = (lastFencePost.transform.position + newFencePost.transform.position) / 2f;
+                    GameObject newFenceConnection = Realtime.Instantiate(fenceConnectionPrefab.name, midPoint, Quaternion.identity,true);
+                    Vector3 direction = lastFencePost.transform.position - newFencePost.transform.position;
+                    newFenceConnection.transform.rotation = Quaternion.LookRotation(direction);
+                    float distance = Vector3.Distance(lastFencePost.transform.position, newFencePost.transform.position);
+                    newFenceConnection.transform.localScale = new Vector3(0.2f, 0.4f, distance);
+
+                    MeshRenderer fenceConnectionRenderer = newFenceConnection.GetComponent<MeshRenderer>();
+                    if (fenceConnectionRenderer != null){
+                        fenceConnectionRenderer.material = fenceMaterial;
+                    }
+                }
+
+                lastFencePost = newFencePost;
+            }
         }
     }
 }
--- XRStarterKit/Assets/XRStarterKit/Scripts/RaycastAreaTarget.cs
+++ XRStarterKit/Assets/XRStarterKit/Scripts/RaycastAreaTarget.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Normal.Realtime;
-public class RaycastAreaTarget : MonoBehaviour{
-    public float maxRaycastDistance = 100.0f;
-    public LayerMask areaTargetLayer;
-    public GameObject prefab;
-    private Realtime _realtime;
-    void Update(){
-        if (Input.GetMouseButtonDown(0)){
-            Vector3 clickPosition = Input.mousePosition;
-            Ray ray = Camera.main.ScreenPointToRay(clickPosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, maxRaycastDistance, areaTargetLayer)){
-                if (hit.collider.GetComponent<MeshCollider>() != null){
-                    Debug.Log("Raycast hit the Area Target's Mesh Collider");
-                    // Instantiate(prefab, hit.point, prefab.transform.rotation);
-                    Realtime.Instantiate(prefab.name, hit.point, prefab.transform.rotation,true);
-                }
-            }
-        }
-    }
-}
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Normal.Realtime;
+// public class RaycastAreaTarget : MonoBehaviour{
+//     public float maxRaycastDistance = 100.0f;
+//     public LayerMask areaTargetLayer;
+//     public GameObject prefab;
+//     private Realtime _realtime;
+//     void Update(){
+//         if (Input.GetMouseButtonDown(0)){
+//             Vector3 clickPosition = Input.mousePosition;
+//             Ray ray = Camera.main.ScreenPointToRay(clickPosition);
+//             RaycastHit hit;
+//             if (Physics.Raycast(ray, out hit, maxRaycastDistance, areaTargetLayer)){
+//                 if (hit.collider.GetComponent<MeshCollider>() != null){
+//                     Debug.Log("Raycast hit the Area Target's Mesh Collider");
+//                     // Instantiate(prefab, hit.point, prefab.transform.rotation);
+//                     Realtime.Instantiate(prefab.name, hit.point, prefab.transform.rotation,true);
+//                 }
+//             }
+//         }
+//     }
+// }
