6ffd16d6306b3c3cf5f2ee5f3cdee51138a293a7
manmythmardo <alexc.mardoqueo@gmail.com>
2023-04-30 01:24:34 -0500

    Normcore Script added to areatarget

--- XRStarterKit/Assets/XRStarterKit/Scripts/RaycastAreaTarget.cs
+++ XRStarterKit/Assets/XRStarterKit/Scripts/RaycastAreaTarget.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Normal.Realtime;
 public class RaycastAreaTarget : MonoBehaviour{
     public float maxRaycastDistance = 100.0f;
     public LayerMask areaTargetLayer;
     public GameObject prefab;
-
+    private Realtime _realtime;
     void Update(){
         if (Input.GetMouseButtonDown(0)){
             Vector3 clickPosition = Input.mousePosition;
@@ -15,7 +15,8 @@ public class RaycastAreaTarget : MonoBehaviour{
             if (Physics.Raycast(ray, out hit, maxRaycastDistance, areaTargetLayer)){
                 if (hit.collider.GetComponent<MeshCollider>() != null){
                     Debug.Log("Raycast hit the Area Target's Mesh Collider");
-                    Instantiate(prefab, hit.point, prefab.transform.rotation);
+                    // Instantiate(prefab, hit.point, prefab.transform.rotation);
+                    Realtime.Instantiate(prefab.name, hit.point, prefab.transform.rotation,true);
                 }
             }
         }
