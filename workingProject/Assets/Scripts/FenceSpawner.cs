e96a427b8e9cd4b9f84187d0fb5d94673ffde8b6
manmythmardo <alexc.mardoqueo@gmail.com>
2023-04-29 21:09:55 -0500

    Fence Spawner Model: Basic Fence
    Currently, this script and prefab spawns a fence based on the user clicks. Will update this further.

---
+++ workingProject/Assets/Scripts/FenceSpawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FenceSpawner : MonoBehaviour
+{
+    public GameObject fencePostPrefab;
+    public GameObject fenceConnectionPrefab;
+    public Material fenceMaterial;
+    public int maxFencePosts = 5;
+    private int fencePostCount = 0;
+    private GameObject lastFencePost = null;
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0) && fencePostCount < maxFencePosts){
+            // Currently gets the position of the mouse click... need to update this to the area target
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            clickPosition.y = 0f; // Spawns it at ground level right now, since we don't have vuforia setup
+
+            // Instantiate a new fence post at the clicked position
+            GameObject newFencePost = Instantiate(fencePostPrefab, clickPosition, Quaternion.identity);
+            MeshRenderer fencePostRenderer = newFencePost.GetComponent<MeshRenderer>();
+            if (fencePostRenderer != null){
+                fencePostRenderer.material = fenceMaterial;
+            }
+            fencePostCount++;
+
+            // If this is not the first fence post ceate a connection to the last one
+            if (lastFencePost != null){
+                Vector3 midPoint = (lastFencePost.transform.position + newFencePost.transform.position) / 2f;
+                GameObject newFenceConnection = Instantiate(fenceConnectionPrefab, midPoint, Quaternion.identity);
+                Vector3 direction = lastFencePost.transform.position - newFencePost.transform.position;
+                newFenceConnection.transform.rotation = Quaternion.LookRotation(direction);
+                float distance = Vector3.Distance(lastFencePost.transform.position, newFencePost.transform.position);
+                newFenceConnection.transform.localScale = new Vector3(0.2f, .4f, distance);
+                MeshRenderer fenceConnectionRenderer = newFenceConnection.GetComponent<MeshRenderer>();
+                if (fenceConnectionRenderer != null){
+                    fenceConnectionRenderer.material = fenceMaterial;
+                }
+            }
+
+            // Sets the last fence point as the new one
+            lastFencePost = newFencePost;
+        }
+    }
+}
