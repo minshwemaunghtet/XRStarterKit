4d8362e582ccee5b479dd0c36f2e88f921c6070e
manmythmardo <84713548+manmythmardo@users.noreply.github.com>
2023-05-08 10:15:17 -0500

    UI Improved

--- XRStarterKit/Assets/XRStarterKit/Scripts/ToggleUI.cs
+++ XRStarterKit/Assets/XRStarterKit/Scripts/ToggleUI.cs
@@ -4,16 +4,21 @@ using UnityEngine.UI;
 public class ToggleUI : MonoBehaviour
 {
     public Button toggleButton;
+    public Button toggleButton2;
     public GameObject uiElement;
+    public GameObject firstUI;
 
     private void Start()
     {
         toggleButton.onClick.AddListener(ToggleUIElement);
+        toggleButton2.onClick.AddListener(ToggleUIElement);
         uiElement.SetActive(false);
+        firstUI.SetActive(true);
     }
 
     private void ToggleUIElement()
     {
         uiElement.SetActive(!uiElement.activeSelf);
+        firstUI.SetActive(!firstUI.activeSelf);
     }
 }
