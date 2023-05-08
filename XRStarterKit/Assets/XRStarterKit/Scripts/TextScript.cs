66f1456c8b7e4b4107b3ae0b0fba342adfae641c
Min Shwe Maung Htet <minshwemaunghtet@gmail.com>
2023-05-08 00:11:45 -0500

    Revert "Merge branch 'main' of https://github.com/minshwemaunghtet/XRStarterKit"
    This reverts commit 3e91c6fc2ab3450913e3a2fd864b9a0bb99493de, reversing
    changes made to 5f351d450be02246bec1fcd02440f6c1d497fb49.

--- XRStarterKit/Assets/XRStarterKit/Scripts/TextScript.cs
+++ XRStarterKit/Assets/XRStarterKit/Scripts/TextScript.cs
@@ -8,25 +8,7 @@ public class TextScript : MonoBehaviour
     public PriceTracker priceTracker;
     public TextMeshProUGUI summaryText;
     public TextMeshProUGUI displayText;
-
-    public TextMeshProUGUI chairText;
-
-    /*Quantity Variables*/
-    public TextMeshProUGUI chairQt;
-    public TextMeshProUGUI tableQt;
-    public TextMeshProUGUI treeQt;
-    public TextMeshProUGUI bushQt;
-    public TextMeshProUGUI concreteQt;
-    public TextMeshProUGUI fenceQt;
     
-
-    /*Price Variables*/
-    public TextMeshProUGUI chairPrice;
-    public TextMeshProUGUI tablePrice;
-    public TextMeshProUGUI treePrice;
-    public TextMeshProUGUI bushPrice;
-    public TextMeshProUGUI concretePrice;
-    public TextMeshProUGUI fencePrice;
     void Update()
     {
         summaryText.text = "Summary of all Items";
@@ -34,10 +16,5 @@ public class TextScript : MonoBehaviour
         displayText.text = "Number of objects: " + priceTracker.TotalNumObjects.ToString() + "\n" 
                           + "Total Price: " + priceTracker.TotalPrice.ToString("C") + "\n" 
                           + "Total number of fence posts: " + priceTracker.numFencePosts.ToString();
-        chairText.text = " Random text";
-
-        chairQt.text= priceTracker.numChairs.ToString();
-        tableQt.text = priceTracker.numTables.ToString();
-        
     }
 }
