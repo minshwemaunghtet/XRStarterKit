cca3b5491d8a14f951599c6a104f3bc7973c967e
David W <40841733+TheDWu@users.noreply.github.com>
2023-05-07 21:00:03 -0500

    Renamed all items

--- XRStarterKit/Assets/XRStarterKit/Scripts/TextScript.cs
+++ XRStarterKit/Assets/XRStarterKit/Scripts/TextScript.cs
@@ -7,12 +7,35 @@ public class TextScript : MonoBehaviour
 {
     public PriceTracker priceTracker;
     public TextMeshProUGUI displayText;
+
+    public TextMeshProUGUI chairText;
+
+    /*Quantity Variables*/
+    public TextMeshProUGUI chairQt;
+    public TextMeshProUGUI tableQt;
+    public TextMeshProUGUI treeQt;
+    public TextMeshProUGUI bushQt;
+    public TextMeshProUGUI concreteQt;
+    public TextMeshProUGUI fenceQt;
     
+
+    /*Price Variables*/
+    public TextMeshProUGUI chairPrice;
+    public TextMeshProUGUI tablePrice;
+    public TextMeshProUGUI treePrice;
+    public TextMeshProUGUI bushPrice;
+    public TextMeshProUGUI concretePrice;
+    public TextMeshProUGUI fencePrice;
     void Update()
     {
         // Update the TextMesh Pro component with the current amount of the Scriptable Object
         displayText.text = "Number of objects: " + priceTracker.TotalNumObjects.ToString() + "\n" 
                           + "Total Price: " + priceTracker.TotalPrice.ToString("C") + "\n" 
                           + "Total number of fence posts: " + priceTracker.numFencePosts.ToString();
+        chairText.text = " Random text";
+
+        chairQt.text= priceTracker.numChairs.ToString();
+        tableQt.text = priceTracker.numTables.ToString();
+        
     }
 }
